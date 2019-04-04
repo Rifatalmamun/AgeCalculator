ff9bcc1ab9d3759e8d737f5bc4aa57cc40cef976
Rifatalmamun <rifatalmamun.cse@gmail.com>
2019-04-05 03:09:12 +0600

    Primary Result

--- app/src/main/java/com/example/rifat/agecalculator/MainActivity.java
+++ app/src/main/java/com/example/rifat/agecalculator/MainActivity.java
@@ -1,13 +1,21 @@
 package com.example.rifat.agecalculator;
 
+import android.app.DatePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.format.DateFormat;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
+
+import java.time.Year;
+import java.util.Calendar;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     // declaration all the variable
@@ -16,6 +24,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private EditText currentDateDay,currentDateMonth,currentDateYear,birthDateDay,birthDateMonth,birthDateYear;
     private Button calculateAgeButton,clearButton,currentDateCalenderPickerButton,birthDateCalenderPickerButton;
 
+    private DatePickerDialog current_Date_Picker;
+    private DatePickerDialog birth_Date_Picker;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -55,6 +65,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         birthDateCalenderPickerButton.setOnClickListener(this);
         calculateAgeButton.setOnClickListener(this);
         clearButton.setOnClickListener(this);
+
+        // First work is to set Current Date ........................
+        Calendar c = Calendar.getInstance();
+        int day = c.get(Calendar.DAY_OF_MONTH);
+        int month = c.get(Calendar.MONTH)+1;
+        int year = c.get(Calendar.YEAR);
+
+        // Current Date er integer variable gulo k String a convert korlam..........
+        String currentDay=Integer.toString(day);
+        String currentMonth=Integer.toString(month);
+        String currentYear=Integer.toString(year);
+        // Current Date k set Korlam....................................................
+        currentDateDay.setText(currentDay);
+        currentDateMonth.setText(currentMonth);
+        currentDateYear.setText(currentYear);
     }
 
     @Override
@@ -66,18 +91,108 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             openBirthDatePicker();
         }
         if(v.getId()==R.id.CalculateAgeButton_id){
+            // take all Current Date day,month,year...................................................
+            String takeCurrentDay=currentDateDay.getText().toString();
+            String takeCurrentMonth=currentDateMonth.getText().toString();
+            String takeCurrentYear=currentDateYear.getText().toString();
+            // take all Birth Date day,month,year.....................................................
+            String takeBirthDay=birthDateDay.getText().toString();
+            String takeBirthMonth=birthDateMonth.getText().toString();
+            String takeBirthYear=birthDateYear.getText().toString();
 
+            if(takeBirthDay.isEmpty() || takeBirthMonth.isEmpty() || takeBirthYear.isEmpty()){
+
+                Toast.makeText(getApplicationContext(), "Please Input DOB", Toast.LENGTH_SHORT).show();
+            }else{
+
+                int cDay=Integer.parseInt(takeCurrentDay);
+                int cMonth=Integer.parseInt(takeCurrentMonth);
+                int cYear=Integer.parseInt(takeCurrentYear);
+                int bDay=Integer.parseInt(takeBirthDay);
+                int bMonth=Integer.parseInt(takeBirthMonth);
+                int bYear=Integer.parseInt(takeBirthYear);
+
+                if(bDay>cDay){
+                    cDay=cDay+30;
+                    bMonth=bMonth+1;
+                }
+                if(bMonth>cMonth){
+                    cMonth=cMonth+12;
+                    bYear=bYear+1;
+                }
+
+                int F_day=(cDay-bDay);
+                int F_month=(cMonth-bMonth);
+                int F_year=(cYear-bYear);
+
+                String finalDay=Integer.toString(F_day);
+                String finalMonth=Integer.toString(F_month);
+                String finalYear=Integer.toString(F_year);
+
+                yearResult.setText(finalYear+" Year");
+                monthResult.setText(finalMonth+" Month");
+                dayResult.setText(finalDay+" Day");
+
+            }
         }
         if(v.getId()==R.id.ClearButton_id){
-
+            birthDateDay.setText("");
+            birthDateMonth.setText("");
+            birthDateYear.setText("");
         }
     }
-    // this is openCurrentDatePicker method.................
+    // this is OpenCurrentDatePicker method....................................
+    //.........................................................................
     private void openCurrentDatePicker() {
 
+        DatePicker datePicker1=new DatePicker(this);
+        int currentDay = datePicker1.getDayOfMonth();
+        int currentMonth = (datePicker1.getMonth());
+        int currentYear = datePicker1.getYear();
+
+        current_Date_Picker=new DatePickerDialog(this,
+
+                new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        //textView.setText(dayOfMonth+"/"+(month+1)+"/"+year);
+                        String PCD=Integer.toString(dayOfMonth);
+                        String PCM=Integer.toString(month+1);
+                        String PCY=Integer.toString(year);
+
+                        currentDateDay.setText(PCD);
+                        currentDateMonth.setText(PCM);
+                        currentDateYear.setText(PCY);
+                    }
+                },currentYear,currentMonth,currentDay);
+
+        current_Date_Picker.show();
     }
-    // this is openBirthDatePicker method...................
+    // this is openBirthDatePicker method......................................
+    //.........................................................................
     private void openBirthDatePicker() {
 
+        DatePicker datePicker2=new DatePicker(this);
+        int birthDay=datePicker2.getDayOfMonth();
+        int birthMonth=datePicker2.getMonth();
+        int birthYear=datePicker2.getYear();
+
+        birth_Date_Picker=new DatePickerDialog(this,
+
+                new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+
+                        String PBD=Integer.toString(dayOfMonth);
+                        String PBM=Integer.toString(month+1);
+                        String PBY=Integer.toString(year);
+
+                        birthDateDay.setText(PBD);
+                        birthDateMonth.setText(PBM);
+                        birthDateYear.setText(PBY);
+                    }
+                },birthYear,birthMonth,birthDay);
+
+        birth_Date_Picker.show();
     }
 }
